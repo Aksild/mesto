87cfd5024022e459232b906d4fa92a49fd13c409
Maxim Tarasov <formbox@inbox.ru>
2021-11-14 19:13:37 +0300

    проектная работа 5

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,164 @@
-const popupElement = document.querySelector(".popup");
+// Переменные попап с именем и профессией
 
+const popupAuthor = document.querySelector("#author");
 const editButton = document.querySelector(".profile__author-edit-button");
-
-const closeButton = document.querySelector(".popup__close-button");
-
-let authorName = document.querySelector(".profile__author-name");
-
+const closeButtons = document.querySelectorAll(".popup__close-button");
+let authortitle = document.querySelector(".profile__author-name");
 let authorText = document.querySelector(".profile__author-text");
+let authorNameInput = document.querySelector(".popup__field-text_author_name");
+let authorInfoInput = document.querySelector(".popup__field-text_author_info");
 
-let nameInput = document.querySelector(".popup__field-text_user_name");
+// Переменные попап с добавлением карточки
 
-let infoInput = document.querySelector(".popup__field-text_user_info");
+const popupCard = document.querySelector("#card");
+const addCardButton = document.querySelector(".profile__add-button");
+const cardTitleInput = document.querySelector(".popup__field-text_card_title");
+const cardPhotoInput = document.querySelector(".popup__field-text_card_photo");
 
-function formSubmitHandler(evt) {
+const photoGridTemplate = document.querySelector("#photo-grid").content;
+const photoGridElementTemplate = photoGridTemplate.querySelector(
+  ".photo-grid__rectangle"
+);
+
+const photoGridSection = document.querySelector(".photo-grid");
+
+// Переменные попап с картинкой
+
+const popupPicture = document.querySelector("#picture");
+
+// Функция открывания попапа
+
+function openPopup(popup) {
+  popup.classList.add("popup_opened");
+}
+
+// Функция закрывания попапа
+
+function closePopups() {
+  popupAuthor.classList.remove("popup_opened");
+  popupCard.classList.remove("popup_opened");
+  popupPicture.classList.remove("popup_opened");
+}
+
+// Функция лайка
+
+// Функция создания карточки с картинкой
+
+function createCard(title, imageUrl) {
+  const photoGridElement = photoGridElementTemplate.cloneNode(true);
+
+  const deleteButton = photoGridElement.querySelector(
+    ".photo-grid__delete-button"
+  );
+  deleteButton.addEventListener("click", function () {
+    const photoCard = deleteButton.closest(".photo-grid__rectangle");
+    photoCard.remove();
+  });
+
+  const likeButton = photoGridElement.querySelector(".photo-grid__like-button");
+
+  function like(card) {
+    card.classList.toggle("photo-grid__like-button_active");
+  }
+
+  likeButton.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    like(likeButton);
+  });
+  const image = photoGridElement.querySelector(".photo-grid__image");
+  image.src = imageUrl;
+  image.alt = title;
+  photoGridElement.querySelector(".photo-grid__text").textContent = title;
+
+  image.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    const popupImage = popupPicture.querySelector(".popup__image");
+    popupImage.src = imageUrl;
+    popupImage.alt = title;
+    popupPicture.querySelector(".popup__description").textContent = title;
+    openPopup(popupPicture);
+  });
+
+  photoGridSection.prepend(photoGridElement);
+}
+
+// Функция лайка карточки
+
+// Присваивание данных с именем и профессией из попапа на страницу
+
+popupAuthor.querySelector("form").addEventListener("submit", function (evt) {
   evt.preventDefault();
 
-  authorName.textContent = nameInput.value;
-  authorText.textContent = infoInput.value;
-  closePopup();
-}
+  authortitle.textContent = authorNameInput.value;
+  authorText.textContent = authorInfoInput.value;
+  closePopups();
+});
 
-popupElement.addEventListener("submit", formSubmitHandler);
+// Присваивание данных с именем и профессией из страницы на попап
 
-function openPopup() {
-  nameInput.value = authorName.textContent;
-  infoInput.value = authorText.textContent;
+editButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  authorNameInput.value = authortitle.textContent;
+  authorInfoInput.value = authorText.textContent;
+  openPopup(popupAuthor);
+});
 
-  popupElement.classList.add("popup_opened");
-}
+// Присваивание данных с картинкой и названием из попапа на созданную карточку
 
-function closePopup() {
-  popupElement.classList.remove("popup_opened");
-}
+popupCard.querySelector("form").addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  createCard(cardTitleInput.value, cardPhotoInput.value);
+  closePopups();
+});
 
-editButton.addEventListener("click", openPopup);
+addCardButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  openPopup(popupCard);
+});
 
-closeButton.addEventListener("click", closePopup);
+// Метод закрывания попапа через обработчик события
+
+closeButtons.forEach((closeButton) => {
+  closeButton.addEventListener("click", closePopups);
+});
+
+// Массив для карточек по умолчанию
+
+const initialCards = [
+  {
+    title: "Архыз",
+    imageUrl:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    title: "Челябинская область",
+    imageUrl:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    title: "Иваново",
+    imageUrl:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    title: "Камчатка",
+    imageUrl:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    title: "Холмогорский район",
+    imageUrl:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    title: "Байкал",
+    imageUrl:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+
+// Метод присвоения фотографии и названия для созданной карточки при создании
+
+initialCards.forEach((cardData) => {
+  createCard(cardData.title, cardData.imageUrl);
+});
